fd8b8f33c1ca670c8a6004323fc983e36af95958
MrZ33 <dhaifallah.zerhoui@gmail.com>
2017-04-28 15:09:40 +0200

    3rd commit

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-$(document).ready(function() {
-
-  // Define the height of your hero here
-  var heroHeight = 800;
-
-  $(window).scroll(function() {
-    if ($(document).scrollTop() < heroHeight) {
-
-      $("#menu").css({
-        "position": "relative",
-      });
-    } else {
-      $("#menu").css({
-        "position": "fixed",
-        "top": "0"
-      });
-    }
-  });
-});
-
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
